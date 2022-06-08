bf655c296cd61bb0dc81ca307f00dab81a8def56
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-08 19:11:25 -0300

    POST Client

--- backend/src/main/java/com/brunoelinton/crudclients/resources/ClientResource.java
+++ backend/src/main/java/com/brunoelinton/crudclients/resources/ClientResource.java
@@ -1,5 +1,6 @@
 package com.brunoelinton.crudclients.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brunoelinton.crudclients.dto.ClientDTO;
 import com.brunoelinton.crudclients.services.ClientService;
@@ -29,5 +33,16 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		ClientDTO clientDTO = service.findById(id);
 		return ResponseEntity.ok().body(clientDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(dto.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
